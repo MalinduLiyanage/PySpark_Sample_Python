729d1c34d601aed9bb6fe814b2990df8580612d0
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-06-05 16:31:33 +0530

    initial commit

---
+++ customers_202506051522.sql
@@ -0,0 +1,113 @@
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('cb816669-351f-45c1-9cd2-bfee70cc9a54','9633 Elm St, Los Angeles',15463.39,'Alice','Smith',600.28,9892.78),
+	 ('e99c1388-5df9-47ee-b5ea-ea34a5278963','2464 Maple Blvd, Chicago',380513.02,'Olivia','Williams',129.38,9339.43),
+	 ('5051db98-429b-4c2f-8839-c2cca04ecb16','1 Pine Ave, Los Angeles',81176.79,'Michael','Garcia',715.95,5502.64),
+	 ('09584401-2cc3-475b-87d5-af9dc3265b62','4643 Maple Blvd, Phoenix',262846.98,'Emily','Davis',441.69,9517.67),
+	 ('e694f6de-ea3d-40bf-b2c0-8682e42db0b8','361 Oak St, New York',762254.45,'John','Williams',342.36,7652.29),
+	 ('56a19b3d-1b9b-4416-aca9-c9dbd2290ca2','1541 Maple Blvd, Chicago',189084.84,'Michael','Smith',932.98,9559.29),
+	 ('3a14840b-a24f-4ff1-8d42-952ae4aca0e5','1775 Maple Blvd, Los Angeles',606081.95,'David','Williams',543.65,5241.69),
+	 ('c0d0534a-cd6e-47a0-869e-2df08d8b6e6f','5060 Elm St, New York',675089.59,'John','Williams',941.71,8413.3),
+	 ('7a119254-fa90-46ef-97a5-4c94acd1cd9b','3286 Maple Blvd, Phoenix',168727.34,'Michael','Garcia',868.08,6211.16),
+	 ('c085f64c-a96f-4a93-8ff1-f7554f12760c','8300 Elm St, Chicago',631782.27,'Olivia','Taylor',651.27,9981.72);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('001106b4-eca2-4dd7-9c94-024621888ef8','1148 Oak St, New York',706468.19,'Charlie','Davis',129.05,7840.35),
+	 ('9f7b6f23-c923-4c27-85d4-7f0b0751d538','8604 Maple Blvd, New York',968805.16,'Olivia','Davis',604.93,5145.2),
+	 ('70ad1e44-ca61-477d-b085-6c5f52c04afe','2805 Pine Ave, New York',550679.1,'Alice','Garcia',261.33,9444.34),
+	 ('5710091b-afb0-47cf-a0d8-188bd139a3dc','8214 Oak St, Los Angeles',376176.71,'Jane','Miller',877.4,6027.39),
+	 ('5d0cffdd-e574-4dc0-94fd-797472c9ce15','2580 Maple Blvd, Los Angeles',739123.47,'David','Johnson',92.82,5892.88),
+	 ('72b38361-62f4-4d46-98cb-d1103a76e593','7077 Oak St, New York',853211.33,'Alice','Smith',315.69,6224.15),
+	 ('e2d6a120-444d-436d-8b48-6b1b12fa1536','5253 Pine Ave, Phoenix',303791.39,'Alice','Garcia',158.91,7383.59),
+	 ('6974cc7e-74ab-4d1f-9d53-d9f4a7ed3d84','1522 Main St, New York',63885.36,'Michael','Williams',854.13,6239.42),
+	 ('39aa710c-7451-4c6a-824f-f110d6069e8d','8494 Oak St, Phoenix',44695.27,'David','Davis',991.79,5855.41),
+	 ('610e765c-b7b2-4bf4-9e90-80e78a9d96d1','1544 Elm St, Houston',574799.48,'Jane','Jones',796.24,6612.55);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('13f6e905-f69f-435b-bf6d-027a72c0aeb8','7592 Oak St, Phoenix',469696.93,'Emily','Williams',170.29,3677.84),
+	 ('a045bef9-b281-4420-a272-ed098e4614cb','3786 Oak St, Los Angeles',744804.54,'Olivia','Doe',199.58,8491.88),
+	 ('5c18aff6-5c5a-4a84-9a1e-fc3b7c0fe092','3902 Oak St, New York',314844.16,'Olivia','Brown',170.57,9021.98),
+	 ('48a04704-1533-4785-bded-f181bf71745d','9564 Pine Ave, Los Angeles',722351.04,'Charlie','Johnson',595.13,9520.67),
+	 ('d652f7b4-2c5c-4ea3-83ec-e604a7958b2e','9849 Maple Blvd, Phoenix',343330.21,'Jane','Taylor',984.3,5051.17),
+	 ('6ab68490-3d3f-4cc0-91cd-cc758709cc75','2248 Maple Blvd, New York',839865.77,'Olivia','Williams',799.24,3323.81),
+	 ('2b78763c-ceed-4a00-8d42-bedc98a1a461','9330 Oak St, Chicago',555194.86,'Jane','Taylor',139.68,6552.5),
+	 ('d5e3cd2b-610b-48f5-9560-23f3b554ede4','9635 Elm St, Houston',390784.68,'Bob','Miller',898.33,5150.91),
+	 ('2176e702-a2e3-4ad9-b637-4f47622b8554','1293 Main St, Chicago',675510.48,'Sophia','Doe',189.9,7455.35),
+	 ('549324a6-37fd-48b6-a824-689128b2745a','2433 Oak St, Phoenix',501835.93,'Olivia','Davis',502.94,5618.59);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('6a35e9e0-59cf-4f92-bd86-2a396ff80915','5050 Main St, New York',930475.79,'Jane','Brown',269.66,5771.22),
+	 ('68d56238-3bc8-4bc9-acf5-19c89353777a','2809 Oak St, Los Angeles',908318.48,'John','Miller',621.73,4867.72),
+	 ('de040c86-0422-4f74-b77f-6aa10d9cc27c','7840 Main St, New York',111917.41,'Emily','Smith',569.39,7307.63),
+	 ('14f7fbdd-fa82-4bb8-901e-21f628f7950f','5999 Main St, Houston',514902.95,'Emily','Taylor',911.94,3216.6),
+	 ('b36b1045-184b-49e7-8bfc-9f463ac35912','3190 Main St, Houston',413022.51,'Jane','Doe',576.27,3504.42),
+	 ('2da63cc1-8e2d-49f9-9c08-bee79772a31f','2539 Maple Blvd, Los Angeles',885284.37,'Jane','Davis',612.99,6039.09),
+	 ('fb6edfff-3187-478d-b0b8-24719cb78fed','2998 Pine Ave, Phoenix',907752.67,'Alice','Garcia',991.61,5457.94),
+	 ('a3bd7c9e-1956-489f-9f8a-ee0d3eeac09f','1156 Pine Ave, New York',145990.58,'John','Taylor',61.99,4767.53),
+	 ('c4fddbb3-d88a-4af6-bbf0-ff424aa9c0c0','9271 Oak St, Los Angeles',535327.35,'Jane','Taylor',813.89,4997.5),
+	 ('5f7dddd9-7486-4353-b7e0-513942a454aa','7953 Oak St, Phoenix',257875.56,'John','Brown',745.43,9664.46);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('2779860d-d659-4272-9c42-52b7978dff95','9107 Main St, Chicago',393769.9,'David','Brown',660.29,9790.8),
+	 ('2bb56d74-6e87-41a7-9040-7b75df5f119a','2273 Main St, Chicago',95015.11,'Emily','Brown',56.12,4513.49),
+	 ('16a9a5f0-2741-4baa-a7ac-e6988df13271','5605 Pine Ave, Los Angeles',857436.38,'Bob','Taylor',969.06,4514.51),
+	 ('6a08d83c-03db-4f00-b1cd-d625ab68b68d','8289 Elm St, Phoenix',193282.73,'Emily','Garcia',861.75,8120.14),
+	 ('0fb80c1c-901c-480f-a375-af28469026d6','9316 Pine Ave, Los Angeles',452172.58,'Michael','Brown',244.79,5282.38),
+	 ('754e9d3c-136d-4c8b-ba12-f7d507496e08','123 Oak St, Los Angeles',75236.43,'John','Jones',627.66,8118.38),
+	 ('3953f8c0-4097-41f8-b996-21f67228be0c','6133 Pine Ave, New York',369825.66,'Bob','Johnson',420.54,9603.33),
+	 ('02387a88-eeb1-48c5-a4cb-cbf65ab7bba5','4217 Pine Ave, Phoenix',689558.9,'Michael','Smith',817.96,7244.67),
+	 ('4a3b79d5-6930-4e2d-b9b5-4cd4a6b6fb9c','1597 Pine Ave, Los Angeles',268023.82,'John','Davis',153.21,6344.02),
+	 ('8adc85dd-05f8-4948-90a6-70ef306af298','9365 Maple Blvd, Chicago',604333.54,'Michael','Johnson',666.24,6445.7);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('e4b0aff5-3869-4bdb-854b-48cd24aca67d','9763 Elm St, Chicago',250843.18,'Charlie','Doe',187.56,4841.27),
+	 ('0e07a711-9338-4507-b340-13f090773157','6238 Elm St, Los Angeles',914312.12,'Jane','Jones',623.35,3015.4),
+	 ('8489da6b-9076-4e4b-bd7c-8c02e383aeb2','39 Main St, Los Angeles',333407.9,'Alice','Doe',137.1,4818.09),
+	 ('685b993f-571d-411c-860a-8ab89db48d8c','3858 Pine Ave, Phoenix',901010.6,'Charlie','Smith',285.31,4007.45),
+	 ('ad347a5c-6c89-4e18-b874-002a24301118','8983 Oak St, Houston',674981.9,'Emily','Johnson',746.17,4457.55),
+	 ('da0c930e-1238-4c68-ac55-6d2f23dbde47','7611 Maple Blvd, Los Angeles',749505.48,'David','Smith',63.65,9220.52),
+	 ('0ae62535-bb04-443c-9b99-48c64e4c805e','8336 Pine Ave, Los Angeles',45218.1,'Jane','Brown',494.71,4550.61),
+	 ('e24d89fe-d2d8-44ed-8330-5c28c439b532','9659 Elm St, New York',845708.49,'Bob','Jones',757.81,7329.79),
+	 ('2c038c58-4074-4616-b2d9-7c1b599fda52','1441 Pine Ave, Phoenix',35767.32,'David','Brown',708.3,9131.28),
+	 ('24aa7a46-b810-4d41-a744-ef02c87df4fe','1204 Main St, Los Angeles',787384.75,'Sophia','Williams',264.87,7837.73);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('f23ae908-1329-4329-b43c-c6eb0c2e282a','9566 Pine Ave, Chicago',354574.77,'Charlie','Miller',789.21,6556.81),
+	 ('b6cccf0c-e90d-4499-be29-d517cb82c965','3795 Elm St, New York',376382.59,'Jane','Doe',89.78,9519.42),
+	 ('926c98a4-47d0-4941-80ed-100c045f501a','8877 Maple Blvd, Phoenix',677720.87,'Olivia','Johnson',367.54,4716.79),
+	 ('9fe9abf9-621c-4e20-bc71-374935c3a597','5838 Maple Blvd, Houston',319594.85,'Charlie','Taylor',500.79,9187.35),
+	 ('a513c743-502e-4751-b3b7-0544f7068b1f','4011 Oak St, Houston',466080.95,'John','Taylor',386.43,7936.1),
+	 ('ff28f413-cac2-4827-9dc2-4fc4b7016597','2838 Elm St, Houston',393683.82,'Olivia','Johnson',297.75,9782.56),
+	 ('4aad02cc-9d56-4d77-8c7f-fd524d950a89','7350 Main St, Phoenix',65304.44,'Michael','Davis',305.75,8200.2),
+	 ('54f8f9ee-b914-4d48-b293-d0f01f4a7d8d','4779 Main St, Chicago',186482.71,'David','Miller',402.61,9629.49),
+	 ('e56d3f24-742e-4f3b-a967-23328622a622','1895 Elm St, Phoenix',527455.45,'Emily','Garcia',535.03,4008.46),
+	 ('d08e4855-67f9-4259-b946-23a2d77cc7ab','9033 Oak St, Phoenix',988351.27,'Emily','Williams',616.94,4292.78);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('7fc8ce80-b21f-4b3c-b638-4bed96a588a6','2216 Maple Blvd, Chicago',105099.35,'David','Taylor',889.59,4997.36),
+	 ('68927642-4b6b-4483-b0ed-bb50cec57b9f','3531 Maple Blvd, Los Angeles',708496.15,'Sophia','Garcia',60.04,4210.6),
+	 ('1422ae11-9fe4-4cb9-9e41-231b4fb705b6','8051 Maple Blvd, Los Angeles',976413.55,'Charlie','Garcia',807.66,5411.79),
+	 ('a75d3e27-f799-4bb8-9e78-88ca3a5d8433','7007 Main St, Houston',996535.31,'Sophia','Williams',557.08,6099.39),
+	 ('7d63882a-8a97-4f62-86a9-b9fcfcff2610','3758 Maple Blvd, Houston',604139.2,'Charlie','Smith',721.08,3778.29),
+	 ('70e68da1-36ef-4654-a6e9-b2c1660d3188','3421 Elm St, Chicago',763662.36,'David','Williams',379.5,8756.06),
+	 ('47150df6-af41-4342-8b24-6ba5c2cfcdf9','5968 Oak St, Los Angeles',301249.79,'John','Brown',729.68,4762.98),
+	 ('b9a7cb68-647c-46da-b697-7ee500272944','3243 Maple Blvd, New York',992926.44,'Charlie','Williams',100.57,8110.49),
+	 ('e2a373c3-01f3-4321-8517-bd8254a14e0f','754 Pine Ave, Phoenix',263018.54,'Michael','Davis',207.93,8174.85),
+	 ('4febe70f-ecd9-4cc6-9695-6a21a2adfe1b','7577 Elm St, Los Angeles',403930.3,'Charlie','Davis',191.05,4259.77);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('faad8779-32f1-4f70-836b-e9a354a7b32c','9757 Elm St, New York',675012.21,'Charlie','Williams',95.01,9966.1),
+	 ('e61a22e1-61a2-489e-93b5-fb0f5206aa75','1053 Maple Blvd, Phoenix',880855.27,'Jane','Williams',806.14,5915.79),
+	 ('76335fbe-717a-45e9-acac-8f8007b3db8d','9362 Elm St, Los Angeles',611642.98,'David','Smith',134.07,4959.54),
+	 ('63c6073c-8af1-414e-85c1-5b80e8b8a41a','2675 Elm St, Los Angeles',63886.02,'Michael','Brown',519.86,3746.47),
+	 ('a3014e1b-b287-4ffe-8fca-77b14e14c519','4996 Oak St, Los Angeles',688935.4,'David','Davis',402.19,3534.56),
+	 ('96a0695c-f890-49eb-b2e2-d0c996817d7c','3552 Oak St, New York',427849.7,'Michael','Smith',197.41,7084.36),
+	 ('6f8d8f6e-f89a-4bba-bf0f-78f2918f7901','3239 Pine Ave, Houston',930835.17,'Jane','Davis',62.29,4868.63),
+	 ('d147c0bd-4c04-4cee-a3f5-c468cf9ffc1a','8719 Maple Blvd, Los Angeles',383583.49,'Charlie','Smith',558.11,6159.33),
+	 ('d10c0acc-6c72-4d83-a973-9210889d1d89','1447 Oak St, Phoenix',561179.79,'Emily','Taylor',533.09,8704.96),
+	 ('392b1ef2-d66b-49c0-8799-f30918a8ed68','5768 Elm St, Phoenix',128669.71,'Charlie','Taylor',318.41,7916.72);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('cc1e1429-acb8-43cc-9c5f-4286bf440e67','6315 Pine Ave, Phoenix',777854.3,'Michael','Miller',172.94,8292.36),
+	 ('10c2e39b-275b-41d2-a0c2-d103c596ec14','9239 Elm St, New York',143164.58,'Bob','Smith',917.92,4208.18),
+	 ('1125910a-60ba-4811-bba2-d7de3bfe225a','8495 Main St, Phoenix',696736.52,'Emily','Smith',253.62,8470.16),
+	 ('1011d0c2-2e47-43a1-b0a6-2f30b7f9035b','3108 Oak St, Los Angeles',655369.65,'Olivia','Doe',481.71,9708.16),
+	 ('d920e993-7616-41cd-81d7-ed157faf3e05','4517 Pine Ave, Chicago',159208.98,'Sophia','Williams',864.96,5307.29),
+	 ('18f478a5-2b59-4b2d-9855-8a18ae2c5d80','6367 Main St, Phoenix',607651.75,'Emily','Williams',755.51,6815.21),
+	 ('86d99ab8-3f72-4fec-8588-cb9131938135','725 Main St, Los Angeles',505158.31,'Sophia','Smith',918.66,7464.95),
+	 ('2bb52b82-4f81-4218-a084-d84caef1d1d1','935 Main St, Los Angeles',445681.62,'Alice','Williams',272.24,7320.36),
+	 ('1f3fcf8a-d218-45c9-bcfd-b0e28bb550a2','295 Maple Blvd, New York',328067.91,'John','Brown',236.42,9913.39),
+	 ('cad8919d-6afa-40dc-905f-014c0d31c70e','7065 Main St, Los Angeles',330486.84,'Alice','Williams',555.62,6277.64);
+INSERT INTO pyspark_keyspace.customers (customer_id,address,assets_value,first_name,last_name,purchase_amount_per_day,salary) VALUES
+	 ('5288902d-16c8-4f4e-bed4-cf2a4e729382','7721 Main St, Phoenix',858278.27,'David','Miller',719.5,9285.01),
+	 ('721ea030-8ba9-4a21-829c-fe8438afcfd4','5781 Maple Blvd, Los Angeles',132526.02,'Jane','Williams',545.31,6954.91);
